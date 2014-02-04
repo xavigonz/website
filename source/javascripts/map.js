320c912599d59f8c3169b7bd25a581e3d08c8c28
jurre <jurrestender@hotmail.com>
2014-02-04 12:07:56 +0100

    fix map.js indentation

--- source/javascripts/map.js
+++ source/javascripts/map.js
@@ -1,42 +1,42 @@
 //= require jquery
 var map;
-    function initialize() {
-      var defactoHQ = new google.maps.LatLng(53.212124,6.57214);
+function initialize() {
+  var defactoHQ = new google.maps.LatLng(53.212124,6.57214);
 
-      var mapOptions = {
-        zoom: 16,
-        center: defactoHQ,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        scrollwheel: false,
-        draggable: true
-      };
-      map = new google.maps.Map(document.getElementById('map-canvas'),
-          mapOptions);
+  var mapOptions = {
+    zoom: 16,
+    center: defactoHQ,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    scrollwheel: false,
+    draggable: true
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+                            mapOptions);
 
-      var image = 'images/defacto-icon.png';
-      var defactoMarker = new google.maps.Marker({
-          position: defactoHQ,
-          map: map,
-          icon: image
-      });
+                            var image = 'images/defacto-icon.png';
+                            var defactoMarker = new google.maps.Marker({
+                              position: defactoHQ,
+                              map: map,
+                              icon: image
+                            });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+$(function() {
+  var pull    = $('#pull');
+  menu    = $('nav ul');
+  menuHeight  = menu.height();
+
+  $(pull).on('click', function(e) {
+    e.preventDefault();
+    menu.slideToggle();
+  });
+
+  $(window).resize(function(){
+    var w = $(window).width();
+    if(w > 320 && menu.is(':hidden')) {
+      menu.removeAttr('style');
     }
-
-    google.maps.event.addDomListener(window, 'load', initialize);
-
-      $(function() {
-        var pull    = $('#pull');
-          menu    = $('nav ul');
-          menuHeight  = menu.height();
-
-        $(pull).on('click', function(e) {
-          e.preventDefault();
-          menu.slideToggle();
-        });
-
-        $(window).resize(function(){
-              var w = $(window).width();
-              if(w > 320 && menu.is(':hidden')) {
-                menu.removeAttr('style');
-              }
-          });
-      });
+  });
+});
