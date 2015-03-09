700a4e38ce4f850885f1e401d747a772fa2b5dae
Matthijs Kuiper <info@matthijskuiper.nl>
2015-03-09 17:27:17 +0100

    javascript instead of coffeescript

--- source/javascripts/map.js
+++ source/javascripts/map.js
@@ -1,26 +1,28 @@
-
 var map;
 
 function mapInit() {
-  var defactoHQ = new google.maps.LatLng(53.212124, 6.57214);
+  var canvas = document.getElementById('map-canvas');
 
-  var mapOptions = {
-    zoom: 16,
-    center: defactoHQ,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    scrollwheel: false,
-    draggable: true
-  };
+  if (canvas) {
+    var defactoHQ = new google.maps.LatLng(53.212124, 6.57214);
+    var mapOptions = {
+      zoom: 16,
+      center: defactoHQ,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      scrollwheel: false,
+      draggable: true
+    };
 
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-  var image = 'images/marker-orange.svg';
-  var defactoMarker = new google.maps.Marker({
-    position: defactoHQ,
-    clickable: false,
-    map: map,
-    icon: image
-  });
+    var image = 'images/marker-orange.svg';
+    var defactoMarker = new google.maps.Marker({
+      position: defactoHQ,
+      clickable: false,
+      map: map,
+      icon: image
+    });
+  }
 }
 
 google.maps.event.addDomListener(window, 'load', mapInit);
