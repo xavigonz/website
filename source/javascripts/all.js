4f3274115816c78b86ee9a3c041bda60459ac047
Matthijs Kuiper <info@matthijskuiper.nl>
2015-03-18 00:45:10 +0100

    responsive header and updated logo

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,19 +4,30 @@
 $(function () {
   'use strict';
 
-  var $window = $(window);
-
-  $('[data-parallax="background"]').each(function () {
-    var $this = $(this);
-    var speed = $this.data('parallax-speed');
-    var offset = $this.data('parallax-offset') || 0;
-
-    $window.on('scroll', function () {
-      var yPos = -($window.scrollTop() / speed);
-      var coords = '50% ' + (yPos + offset) + 'px';
-      $this.css('backgroundPosition', coords);
+  // Mobile navigation
+  var $menu = $('#header-menu');
+  var $menuToggle = $('#mobile-menu-toggle');
+  $menuToggle.on('click', function (event) {
+    event.preventDefault();
+    $menu.slideToggle(200, function () {
+      if($menu.is(':hidden')) {
+        $menu.removeAttr('style');
+      }
     });
-
-    $window.scroll()
   });
+
+  // var $window = $(window);
+  // $('[data-parallax="background"]').each(function () {
+  //   var $this = $(this);
+  //   var speed = $this.data('parallax-speed');
+  //   var offset = $this.data('parallax-offset') || 0;
+
+  //   $window.on('scroll', function () {
+  //     var yPos = -($window.scrollTop() / speed);
+  //     var coords = '50% ' + (yPos + offset) + 'px';
+  //     $this.css('backgroundPosition', coords);
+  //   });
+
+  //   $window.scroll()
+  // });
 });
