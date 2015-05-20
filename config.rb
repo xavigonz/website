75ff1fadad1e441d29c6bdab71b8af3839a98c4d
Matthijs Kuiper <info@matthijskuiper.nl>
2015-05-20 14:49:49 +0200

    mardown code blocks

--- config.rb
+++ config.rb
@@ -1,6 +1,3 @@
-# Used to build XML files
-require "builder"
-
 ##
 # Compass
 ###
@@ -17,10 +14,10 @@ require "builder"
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", :layout => false
+# page "/path/to/file.html", layout: false
 #
 # With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
+# page "/path/to/file.html", layout: :otherlayout
 #
 # A path which all have the same layout
 # with_layout :admin do
@@ -28,14 +25,69 @@ require "builder"
 # end
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+# activate :livereload
+# activate :livereload, host: "127.0.0.1"
+
+activate :i18n, mount_at_root: :nl, langs: [:nl, :en]
+activate :directory_indexes
+
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+set :relative_links, true
+
+# Middleman syntax (https://github.com/middleman/middleman-syntax)
+activate :syntax #, line_numbers: true
+
+set :markdown_engine, :kramdown
+set :markdown, input: 'GFM', auto_ids: false
+
+#set :markdown_engine, :redcarpet
+#set :markdown, fenced_code_blocks: true, smartypants: true
+
+# Blog
+Time.zone = "CET"
+
+activate :blog do |blog|
+  blog.prefix = "blog"
+  blog.permalink = ":title"
+  blog.tag_template = "blog/tag.html"
+  blog.calendar_template = "blog/calendar.html"
+end
+
+page "blog/*", layout: :blog_post_layout
+page "blog/index.html", layout: :blog_layout
+page "blog/feed.xml", layout: false
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
 
 ###
 # Helpers
 ###
 
-# Methods defined in the helpers block are available in templates
 helpers do
   # Prevent page_classes from prefixing locales
   def page_classes(path=current_path.dup, options={})
@@ -43,12 +95,13 @@ helpers do
   end
 
   def markitdown(string)
-    Kramdown::Document.new(string).to_html
+    Kramdown::Document.new(string, config[:markdown]).to_html
+    # Redcarpet::Markdown.new(Redcarpet::Render::HTML, config[:markdown]).render(string)
   end
 
   def nav_link_to(link_text, url, options={})
     options[:class] ||= ""
-    options[:class] << " active" if url_for(url, :relative => false) == url_for(current_page.url, :relative => false)
+    options[:class] << " active" if url_for(url, relative: false) == url_for(current_page.url, relative: false)
     link_to(link_text, url, options)
   end
 
@@ -77,52 +130,4 @@ helpers do
     lang = options[:language] ? options[:language] : I18n.locale.to_s
     "/#{lang}/#{path}"
   end
-end
-
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
-
-set :relative_links, true
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# activate :livereload
-# activate :livereload, :host => "127.0.0.1"
-
-activate :i18n, :mount_at_root => :nl, :langs => [:nl, :en]
-
-Time.zone = "CET"
-
-page "blog/*", :layout => :blog_post_layout
-page "blog/index.html", :layout => :blog_layout
-page "blog/feed.xml", layout: false
-
-activate :blog do |blog|
-  blog.prefix = "blog"
-  blog.permalink = ":title"
-  blog.tag_template = "blog/tag.html"
-  blog.calendar_template = "blog/calendar.html"
-end
-
-activate :directory_indexes
-
-# Build-specific configuration
-configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
 end
