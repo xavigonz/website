a744dc785f979725eb0f04fcc56303f5384f9b01
Matthijs Kuiper <info@matthijskuiper.nl>
2015-05-28 17:15:04 +0200

    optimize javascript

--- source/javascripts/_map.js
+++ source/javascripts/_map.js
@@ -1,34 +1,36 @@
-var map;
-var mapLocation = new google.maps.LatLng(53.212124, 6.57214);
+if (window.google && window.google.maps) {
+  var map;
+  var mapLocation = new google.maps.LatLng(53.212124, 6.57214);
 
-function mapInit () {
-  var mapCanvas = document.getElementById('map-canvas');
+  function mapInit () {
+    var mapCanvas = document.getElementById('map-canvas');
 
-  if (mapCanvas) {
-    var mapOptions = {
-      zoom: 16,
-      center: mapLocation,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      scrollwheel: false,
-      draggable: true
-    };
+    if (mapCanvas) {
+      var mapOptions = {
+        zoom: 16,
+        center: mapLocation,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        scrollwheel: false,
+        draggable: true
+      };
 
-    map = new google.maps.Map(mapCanvas, mapOptions);
+      map = new google.maps.Map(mapCanvas, mapOptions);
 
-    var mapMarker = new google.maps.Marker({
-      position: mapLocation,
-      clickable: false,
-      map: map,
-      icon: '/images/marker-orange.svg'
-    });
+      var mapMarker = new google.maps.Marker({
+        position: mapLocation,
+        clickable: false,
+        map: map,
+        icon: '/images/marker-orange.svg'
+      });
 
-    mapCenter();
-    google.maps.event.addDomListener(window, 'resize', mapCenter);
+      mapCenter();
+      google.maps.event.addDomListener(window, 'resize', mapCenter);
+    }
   }
-}
 
-function mapCenter () {
-  map.setCenter(mapLocation);
-}
+  function mapCenter () {
+    map.setCenter(mapLocation);
+  }
 
-google.maps.event.addDomListener(window, 'load', mapInit);
+  google.maps.event.addDomListener(window, 'load', mapInit);
+}
